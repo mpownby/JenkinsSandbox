78ba1fbcb86f3387dd391adbef0ebd1870b6cb1a
mpownby <mpownby@users.noreply.github.com>
2020-06-02 15:59:28 -0600

    moved logging into writer

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -20,33 +20,33 @@ def call(Map config=[:]) {
                     writer.writeLine('\t' + file.name + '\t' + file.length());
                 }
         }
-    }
-    
-    def date = new Date();
-    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-    echo "Date and time is: " + sdf.format(date);
-    
-    echo "Build number is: ${BUILD_NUMBER}"
-    
-    def changeLogSets = currentBuild.changeSets;
-    
-    for (change in changeLogSets)
-    {
-        def entries = change.items;
-        for (entry in entries)
+        
+        def date = new Date();
+        def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        writer.writeLine("Date and time is: " + sdf.format(date));
+
+        writer.writeLine("Build number is: ${BUILD_NUMBER}");
+
+        def changeLogSets = currentBuild.changeSets;
+
+        if (config.changes != "false")
         {
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}";
-            for (file in entry.affectedFiles) {
-                echo "   ${file.editType.name} ${file.path}";
+            for (change in changeLogSets)
+            {
+                def entries = change.items;
+                for (entry in entries)
+                {
+                    writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+                    for (file in entry.affectedFiles) {
+                        writer.writeLine("   ${file.editType.name} ${file.path}");
+                    }
+
+                }
             }
-                                                            
-        }
+        } // end if changes to be logged
+            
     }
-    
-    if (config.changes != "false") {
-        echo "changes";
-    }
-    
+        
     echo "Config contents:"
     for (element in config)
     {
